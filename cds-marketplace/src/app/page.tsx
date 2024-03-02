094b2ec0bff322b4a6e85dbcae0de2ac8b5f55f3
911-Berescu-Adrian <adrian.berescu@stud.ubbcluj.ro>
2024-03-02 16:00:22 +0200

    setup nextjs

---
+++ cds-marketplace/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20 py-10 text-center flex-flex-col items-center">
+            <h1 className="text-4xl font-bold">CDs Marketplace</h1>
+        </div>
+    );
+}
